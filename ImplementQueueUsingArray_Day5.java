523d8183bea52417d3dbf889899e045c118c6473
ImSowmya <sowmya.reachout@gmail.com>
2023-12-10 00:37:46 +0530

    Day 5

--- ImplementQueueUsingArray_Day5.java
+++ ImplementQueueUsingArray_Day5.java
@@ -1,28 +1,28 @@
-class ImplementQueueUsingArray_Day5 
-{
-    public static void main(String[] args) 
-    {
-        int [] arr = new int[10000];
-        int front, rear;
+// class ImplementQueueUsingArray_Day5 
+// {
+//     public static void main(String[] args) 
+//     {
+//         int [] arr = new int[10000];
+//         int front, rear;
 
-        ImplementQueueUsingArray_Day5()
-        {
-            front = -1;
-            rear = -1;
-        }
+//         ImplementQueueUsingArray_Day5()
+//         {
+//             front = -1;
+//             rear = -1;
+//         }
 
-        void push (int x)
-        {
-            if (rear == arr.length-1) return;
-            if (rear == -1) front++;
-            arr[++rear] = x;
-        }
+//         void push (int x)
+//         {
+//             if (rear == arr.length-1) return;
+//             if (rear == -1) front++;
+//             arr[++rear] = x;
+//         }
 
-        int pop ()
-        {
-            if(rear==-1 || front>rear) return -1;
-            return arr[front++]
-        }
+//         int pop ()
+//         {
+//             if(rear==-1 || front>rear) return -1;
+//             return arr[front++]
+//         }
         
-    }    
-}
+//     }    
+// }
