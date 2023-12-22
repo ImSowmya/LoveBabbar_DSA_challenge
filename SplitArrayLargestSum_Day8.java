5a29a2d54fe0c584d1b81f41bd602c18d6505a94
ImSowmya <sowmya.reachout@gmail.com>
2023-12-22 12:00:26 +0530

    Day 8

---
+++ SplitArrayLargestSum_Day8.java
@@ -0,0 +1,33 @@
+//
+class SplitArrayLargestSum_Day8
+{
+    public static void main(String[] args) {
+        int [] nums ={7,2,5,10,8};
+        int k = 2;
+
+        int start = 0, end = 0;
+        for (int num : nums)
+        {
+            start = Math.max(start, num);
+            end += num;
+        }
+        while (start < end)
+        {
+            int mid = (start+end)/2;
+            int pieces = 1;
+            int sum = 0;
+            for (int num : nums)
+            {
+                if(num+sum > mid)
+                {
+                    sum = num;
+                    pieces++;
+                }
+                else sum +=num;
+            }
+            if(pieces>k) start = mid+1;
+            else end = mid;
+        }
+        System.out.println(start);  
+    }
+}
