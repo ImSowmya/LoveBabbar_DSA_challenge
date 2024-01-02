608d3758b04b6b2278496af2b49db1be9b0755fe
ImSowmya <sowmya.reachout@gmail.com>
2024-01-02 13:37:14 +0530

    Algorithm

--- ALGO_MergeSort.java
+++ ALGO_MergeSort.java
@@ -5,7 +5,7 @@ class ALGO_MergeSort
     public static void main(String[] args) 
     {
         int [] arr = {3, 87, 125, 2, 0, -9};
-        mergeSort(arr);
+        mergeSort(arr, 0, arr.length);
         System.out.println(Arrays.toString(arr));  
     }
 
@@ -22,6 +22,17 @@ class ALGO_MergeSort
 
         merge(arr, left, right);
     }
+
+    static void mergeSort( int arr [] , int start, int end)
+    {
+        if (end-start == 1) return;
+
+        int mid = (start+end)/2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid, end);
+        merge(arr, start, mid, end);
+    }
+    
 
     static void merge (int [] arr, int [] left, int [] right)
     {
@@ -54,6 +65,46 @@ class ALGO_MergeSort
             arr[k] = right[j];
             j++;
             k++;
+        }
+    }
+
+    static void merge (int [] arr, int start, int mid, int end)
+    {
+        int mix [] = new int[end-start];
+        int k = 0;
+        int i = start;
+        int j = mid;
+        while (i < mid && j < end )
+        {
+            if (arr[i] < arr[j])
+            {
+                mix[k] = arr[i];
+                k++;
+                i++;
+            }
+            else
+            {
+                mix[k] = arr[j];
+                k++;
+                j++;
+            }
+        }
+        while(i < mid)
+        {
+            mix[k] = arr[i];
+            k++;
+            i++;
+        }
+        while(j < end)
+        {
+            mix[k] = arr[j];
+            k++;
+            j++;
+        }
+
+        for (int l = 0; l<mix.length; l++)
+        {
+            arr[start+l] = mix[l];
         }
     }
     
